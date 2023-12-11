3643feb6e6ac538e6c469cb442d4bb5c7c13d060
Aleksander Grüntal <120027774+AleksanderGruntal@users.noreply.github.com>
2023-12-11 11:19:29 +0200

    Update LOGITpv23_kood.py

--- LOGITpv23_kood.py
+++ LOGITpv23_kood.py
@@ -1,3 +1,37 @@
+from random import *
+from math import *
+#5
+a=float(input("pikkus: "))
+b=float(input("laius: "))
+d=sqrt(a**2+b**2)
+print("Diagonaal=",d,"m")
+
+#4
+u=float(input("Ümbermõõt: ")) #L=pi*2*r=pi*d
+d=round(u/pi,2)
+print("Läbimõõt =",d)
+
+
+
+#3
+kokku=randint(1,100)
+print("Laual peal on",kokku,"kommid. Mitu tahad süüa?")
+kommid=int(input())
+kokku-=kommid
+print("Nüüd kokku on",kokku)
+
+
+#2
+vanus=18
+eesnimi="Jaak"
+pikkus=16.5
+kas_käib_koolis=True
+
+print("Muutuja vanus=",vanus,"on", type(vanus))
+print("Muutuja eesnimi=",eesnimi,"on", type(eesnimi))
+print("Muutuja pikkus=",pikkus,"on", type(pikkus))
+print("Muutuja kas_käib_koolis=",kas_käib_koolis,"on", type(kas_käib_koolis))
+
 arv1=float(input("Arv1: "))
 t=input("Tehe: ")
 arv2=float(input("Arv2: "))
